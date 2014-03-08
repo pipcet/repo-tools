888bb1187ac854203ee1ae5dcdb65606cbbfe8dd
Philip <noemail>
2014-03-08 01:15:19 +0000

    support outputting just the shas of recent changes, to feed into repo-overlay.pl --apply

--- repo-log.pl
+++ repo-log.pl
@@ -1,4 +1,13 @@
 #!/usr/bin/perl
+
+use Getopt::Long qw(:config auto_version auto_help);
+
+my $do_just_shas;
+
+GetOptions(
+    "just-shas!"=>\$do_just_shas,
+    );
+
 package RepoStream;
 
 sub peek1 {
@@ -21,6 +30,9 @@ sub peek1 {
 	if ($line =~ /^\.\.CommitDate:[ \t]*(.*)$/) {
 	    $h->{commit}->{date} = $1;
 	    $h->{commit}->{rawdate} = `date -d '$1' +'%s'`;
+	}
+	if ($line =~ /^\.\.SHA:[ \t]*(.*)$/) {
+	    $h->{commit}->{sha} = $1;
 	}
     } else {
 	$h->{eof} = 1;
@@ -68,7 +80,9 @@ sub new {
     $h->{commits} = [];
     $h->{n} = 0;
     $h->{repo} = $repo;
-    open($h->{fh}, "(cd '$repo'; git log -p -m --first-parent --pretty=tformat:'* $repo %h by %an at %ci%n..CommitDate:%ci%n..%N%n..%s%b' --since='1 week ago' --date=iso)|") or die;
+    my $tformat;
+    $tformat = "* $repo %h by %an at %ci%n..CommitDate:%ci%n..%N%n..%s%b%n..SHA:%h";
+    open($h->{fh}, "(cd '$repo'; git log -p -m --first-parent --pretty=tformat:'$tformat' --since='1 week ago' --date=iso)|") or die;
 
     return bless($h, $class);
 }
@@ -94,17 +108,21 @@ while(1) {
     my @dates = sort { $b->[0] <=> $a->[0] } map { [$_->[0]{rawdate}, $_->[0], $_->[1]] } grep { defined($_->[0]) } map { [$_->peek, $_] } values(%r);
 
     if (scalar(@dates)) {
-	my $repo = $dates[0][2]->{repo};
-	my $raw = $dates[0][2]->get->{content};
-	my $cooked = $raw;
-	$cooked =~ s/^(diff --git a\/([^ \t]*))/\*\* $repo$2\n$1/msg;
-	$cooked =~ s/\n+\*\*/\n\*\*/msg;
-	$cooked =~ s/^\.\.\n//msg;
-	my $l;
-	if (($l = length($cooked)) > 1000000) {
-	    $cooked = substr($cooked, 0, 1000) . "\n" . ($l-1000) . " bytes skipped\n"
+	if ($do_just_shas) {
+	    print $dates[0][2]->get->{sha} . "\n";
+	} else {
+	    my $repo = $dates[0][2]->{repo};
+	    my $raw = $dates[0][2]->get->{content};
+	    my $cooked = $raw;
+	    $cooked =~ s/^(diff --git a\/([^ \t]*))/\*\* $repo$2\n$1/msg;
+	    $cooked =~ s/\n+\*\*/\n\*\*/msg;
+	    $cooked =~ s/^\.\.\n//msg;
+	    my $l;
+	    if (($l = length($cooked)) > 1000000) {
+		$cooked = substr($cooked, 0, 1000) . "\n" . ($l-1000) . " bytes skipped\n"
+	    }
+	    print $cooked;
 	}
-	print $cooked;
     } else {
 	last;
     }
